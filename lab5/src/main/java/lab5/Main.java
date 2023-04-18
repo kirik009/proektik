823a08de3f5abb41c14ed00d416356a066acec22
kirillboravik69 <kirillboravik69@gmail.com>
2023-04-18 17:43:52 +0300

    hom

--- lab5/src/main/java/lab5/Main.java
+++ lab5/src/main/java/lab5/Main.java
@@ -4,7 +4,7 @@ import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-
+//erer
 @SpringBootApplication
 public class Main {		
 	/**
